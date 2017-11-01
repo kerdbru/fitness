e47bae9ba8367efe414b63995561272b0f51eb94
Keith Erdbruegger <kerdbruegger0@gmail.com>
2017-11-01 14:58:31 -0500

    change workout description again

--- includes/DbOperation.php
+++ includes/DbOperation.php
@@ -52,20 +52,19 @@ class DbOperation {
 
         $stmt->execute();
 
-        echo "HERE";
         $result = $stmt->get_result();
         while($row = mysqli_fetch_assoc($result)) {
 
-//            $stmt = $this->conn->prepare('SELECT count(*) as number from ratings where workout_description_id = ?');
-//            $stmt->bind_param('i', row['id']);
-//            $stmt->execute();
-//            $row['rating_count'] = $stmt->get_result()['number'];
-//
-//
-//            $stmt = $this->conn->prepare('SELECT sum(score) as number from ratings where workout_description_id = ?');
-//            $stmt->bind_param('i', row['id']);
-//            $stmt->execute();
-//            $row['rating_sum'] = $stmt->get_result()['number'];
+            $stmt = $this->conn->prepare('SELECT count(*) as number from ratings where workout_description_id = ?');
+            $stmt->bind_param('i', row['id']);
+            $stmt->execute();
+            $row['rating_count'] = $stmt->get_result()['number'];
+
+
+            $stmt = $this->conn->prepare('SELECT sum(score) as number from ratings where workout_description_id = ?');
+            $stmt->bind_param('i', row['id']);
+            $stmt->execute();
+            $row['rating_sum'] = $stmt->get_result()['number'];
 
             $rows[] = $row;
         }
