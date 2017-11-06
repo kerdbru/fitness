240f8e06238fa0d87e73ac6cce26a2450060d939
Keith Erdbruegger <kerdbruegger0@gmail.com>
2017-11-05 20:55:17 -0600

    favorite

--- api/addfavorite.php
+++ api/addfavorite.php
@@ -4,6 +4,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $account_id = $_POST['accountId'];
     $workout_id = $_POST['workoutId'];
 
+    echo gettype($account_id);
+
     require_once '../includes/DbOperation.php';
     $db = new DbOperation();
     echo $db->add_favorite($account_id, $workout_id);
