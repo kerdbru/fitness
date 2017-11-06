bcfc9f10c6ea391aa45caf7318a51ec8c8b7c6c8
Keith Erdbruegger <kerdbruegger0@gmail.com>
2017-11-05 20:50:40 -0600

    favorite

--- api/addfavorite.php
+++ api/addfavorite.php
@@ -3,6 +3,7 @@
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $account_id = $_POST['accountId'];
     $workout_id = $_POST['workoutId'];
+    echo $workout_id;
 
     require_once '../includes/DbOperation.php';
     $db = new DbOperation();
