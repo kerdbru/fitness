1215ee51a6c7aff7d491fdf26aa917ea86efb6b5
Keith Erdbruegger <kerdbruegger0@gmail.com>
2017-11-15 13:20:44 -0600

    get exercises

--- api/getexercises.php
+++ api/getexercises.php
@@ -3,6 +3,7 @@
 $response = array();
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
     $search = $_GET["search"];
+    echo "here".$search
 
     require_once '../includes/DbOperation.php';
     $db = new DbOperation();
