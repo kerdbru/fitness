cd1d82c3901b8f61f228da7d6f9bfb71125e1eab
Keith Erdbruegger <kerdbruegger0@gmail.com>
2017-11-21 19:07:50 -0600

    add workout item

--- includes/DbOperation.php
+++ includes/DbOperation.php
@@ -244,7 +244,7 @@ class DbOperation {
             $weight = null;
         }
         $stmt = $this->conn->prepare('INSERT INTO workout_order($workout_description_id, $account_id, $position, \
-                                      $exercise_id, $label_id, $amount, $weight, $sets) VALUES(?,?,?,?,?,?,?,?,?)');
+                                      $exercise_id, $label_id, $amount, $weight, $sets) VALUES(?,?,?,?,?,?,?,?)');
         echo 'HERE';
         $stmt->bind_param('iiiiiiii', $workout_id, $account_id, $position, $exercise_id, $label_id, $amount, $weight, $sets);
         echo 'HERE';
