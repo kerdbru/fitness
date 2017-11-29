6a67a04955ca99f99937825c8b86b1cf03e1c48d
Keith Erdbruegger <kerdbruegger0@gmail.com>
2017-11-28 23:10:09 -0600

    get workouts

--- api/getworkoutdescriptions.php
+++ api/getworkoutdescriptions.php
@@ -5,10 +5,11 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
     $search = $_GET["search"];
     $type = $_GET["type"];
     $id = $_GET["accountId"];
+    $favorite = $_GET["favorite"];
 
     require_once '../includes/DbOperation.php';
     $db = new DbOperation();
-    $response = $db->get_workout_descriptions($search, $type, $id);
+    $response = $db->get_workout_descriptions($search, $type, $id, $favorite);
 }
 
 echo $response;
